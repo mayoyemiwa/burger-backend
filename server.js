a054009ec95f06594de74881a6452ae8583192f7
mayoyemiwa <mayoyemiwa@gmail.com>
2022-12-21 04:56:39 +0100

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.set('trust-proxy', 1)
 app.use(cors())
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
